9ecb9c7f07989522a2b2d9100f371668ea2023ca
max200pl <max2000pl@gmail.com>
2024-08-08 09:46:07 +0300

    #init first a C# project

--- Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb1ed057b6c9283989e0c2f23aa4b4e10032249")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
