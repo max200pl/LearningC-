3a8fad564ae5f13295e189b6cc7b1c9783cb53c8
max200pl <max2000pl@gmail.com>
2024-08-08 10:43:56 +0300

    #learning array collections

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Random dice = new Random(0);
+﻿Random dice = new Random(0);
 int roll = dice.Next(1, 7);
 Console.WriteLine(roll);
 
@@ -10,5 +9,61 @@ orders[1] = "Second Order";
 orders[2] = "Third Order";
 
 
-string[] newOrder = { "Fourth Order", "Fifth Order", "Sixth Order" };
+string[] _ = { "Fourth Order", "Fifth Order", "Sixth Order" };
 
+foreach (string order in orders)
+{
+    Console.WriteLine(order);
+}
+
+int[] inventory = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+int sum = 0;
+int bin = 0;
+foreach (int item in inventory)
+{
+    sum += item;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {item}");
+}
+
+Console.WriteLine(sum);
+
+
+string[] orderIDs = {
+    "A123",
+    "B123",
+    "C234",
+    "A345",
+    "C15",
+    "B177",
+    "G3003",
+    "C235",
+    "B179",
+};
+
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
+
+string str = "Hello World";
+
+char[] charMessage = str.ToCharArray();
+
+Array.Reverse(charMessage);
+
+int x = 0;
+
+foreach (char i in charMessage)
+{
+    if (i == '0') x++;
+}
+
+string new_message = new string(charMessage);
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
