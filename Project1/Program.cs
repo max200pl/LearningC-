1fb1ed057b6c9283989e0c2f23aa4b4e10032249
max200pl <max2000pl@gmail.com>
2024-08-08 09:44:08 +0300

    first commit

---
+++ Project1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random dice = new Random(0);
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+
+string[] orders = new string[3];
+
+orders[0] = "First Order";
+orders[1] = "Second Order";
+orders[2] = "Third Order";
+
+
+string[] newOrder = { "Fourth Order", "Fifth Order", "Sixth Order" };
+
