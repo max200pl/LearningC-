63b3134fa3237f4bdf2b45736bda3498e0228e07
max200pl <max2000pl@gmail.com>
2024-08-08 10:44:29 +0300

    #some updates

--- Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ecb9c7f07989522a2b2d9100f371668ea2023ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8fad564ae5f13295e189b6cc7b1c9783cb53c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
