db340a2bd96c484b913357d7269aea70231e8321
max200pl <max2000pl@gmail.com>
2024-08-22 10:19:58 +0300

    Project 2 added validation for inputs

--- Project_2_Form_Material_design/MainWindow.xaml.cs
+++ Project_2_Form_Material_design/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace Project_2_Form_Material_design
         {
             InitializeComponent();
         }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textboxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string passRepeat = passBoxRepeat.Password.Trim();
+            string email = textboxEmail.Text.Trim().ToLower();
+
+            if (login.Length < 5)
+            {
+                textboxLogin.ToolTip = "This Login incorrect";
+                textboxLogin.Background = Brushes.LightCoral;
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Pass must contain more then 5 simbols";
+                passBox.Background = Brushes.LightCoral;
+            }
+            else if (pass != passRepeat)
+            {
+                passBox.ToolTip = "Pass must contain the same password";
+                passBox.Background = Brushes.LightCoral;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textboxEmail.ToolTip = "Incorrect email";
+                textboxEmail.Background = Brushes.LightCoral;
+            }
+            else
+            {
+                textboxLogin.ToolTip = "";
+                textboxLogin.Background = Brushes.LightCyan;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.LightCyan;
+                passBoxRepeat.ToolTip = "";
+                passBoxRepeat.Background = Brushes.LightCyan;
+                textboxEmail.ToolTip = "";
+                textboxEmail.Background = Brushes.LightCyan;
+
+                MessageBox.Show("Login Succssesfull");
+            }
+        }
     }
 }
